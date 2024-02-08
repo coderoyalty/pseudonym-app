09c961ce57bf5b8c697ed764206cdd7a878bbad5
Akanni Emmanuel <102362366+coderoyalty@users.noreply.github.com>
2024-02-08 16:25:39 +0100

    feat: make header responsive (#4)
    * feat(frontend): update the navbar responsiveness
    
    Closes #3
    
    * chore(frontend-deps): update package.json
    
    - add framer-motion for animation
    
    * feat(frontend): add animation to the responsive navigation menu
    
    * bug(frontend): fix menu-button div not hidden and remove its animation

--- app/src/components/header/header.tsx
+++ app/src/components/header/header.tsx
@@ -1,15 +1,24 @@
 import type { FC } from "react";
 import { Button } from "../ui/button";
+import { HamburgerMenuIcon } from "@radix-ui/react-icons";
+import { IconButton } from "@radix-ui/themes";
+import { motion, AnimatePresence } from "framer-motion";
+import React from "react";
 
 interface NavBarProps extends React.ComponentPropsWithoutRef<"nav"> {}
 
-const NavBar: FC<NavBarProps> = () => {
+interface NavLinksProps {
+  autoHide?: boolean;
+}
+
+const NavLinks: React.FC<NavLinksProps> = ({ autoHide = false }) => {
   return (
-    <nav className="flex justify-between items-center">
-      <a href="/" className="text-2xl font-semibold uppercase font-mono">
-        pseudonym
-      </a>
-      <ul className="flex gap-4 items-center justify-center text-lg">
+    <>
+      <ul
+        className={`flex gap-4 items-center justify-center max-md:flex-col text-lg ${
+          autoHide && "max-md:hidden"
+        }`}
+      >
         <li>
           <a
             href="/"
@@ -35,7 +44,72 @@ const NavBar: FC<NavBarProps> = () => {
           </a>
         </li>
       </ul>
-      <Button>Get Started</Button>
+
+      <Button className={autoHide ? "max-md:hidden" : ""}>Get Started</Button>
+    </>
+  );
+};
+
+const NavBar: FC<NavBarProps> = () => {
+  const [_size, setWindowSize] = React.useState([
+    window.innerWidth,
+    window.innerHeight,
+  ]);
+  const [isOpen, setOpen] = React.useState(false);
+
+  const callback = () => {
+    setWindowSize([window.innerWidth, window.innerHeight]);
+
+    if (window.innerWidth > 768 && isOpen) {
+      setOpen(false);
+    }
+  };
+  React.useEffect(() => {
+    addEventListener("resize", callback);
+
+    return () => {
+      removeEventListener("resize", callback);
+    };
+  }, [callback]);
+
+  return (
+    <nav className="flex justify-between items-center max-md:flex-wrap">
+      <a href="/" className="text-2xl font-semibold uppercase font-mono">
+        pseudonym
+      </a>
+
+      <NavLinks autoHide />
+
+      <IconButton
+        size="2"
+        color="amber"
+        radius="medium"
+        variant="surface"
+        className="cursor-pointer md:hidden"
+        onClick={() => {
+          setOpen(!isOpen);
+        }}
+      >
+        <HamburgerMenuIcon width={"22"} height={"22"} />
+      </IconButton>
+      <AnimatePresence>
+        {isOpen && (
+          <motion.div
+            initial={{ scale: 0 }}
+            animate={{ scale: 1 }}
+            transition={{
+              type: "spring",
+              stiffness: 260,
+              damping: 20,
+            }}
+            className="md:hidden w-full"
+          >
+            <div className="w-[70%] my-4 mx-auto flex flex-col gap-4 justify-center">
+              <NavLinks />
+            </div>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </nav>
   );
 };

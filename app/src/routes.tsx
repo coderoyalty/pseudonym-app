1593214047d73203a25735ea55b5b05abf53865c
Akanni Emmanuel <akannie33@gmail.com>
2024-02-09 14:28:54 +0100

    feat(frontend): add 404 error page for unmatched routes

--- app/src/routes.tsx
+++ app/src/routes.tsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom";
-import HomePage from "./components/home-page/home-page";
+import HomePage from "./pages/home-page";
 import AuthPage from "./pages/AuthPage";
+import NotFoundPage from "./pages/404";
 
 export default function Router() {
   return (
     <>
       <Routes>
+        <Route path="*" element={<NotFoundPage />} />
         <Route path="/" element={<HomePage />} />
         <Route path="/auth" element={<AuthPage />} />
       </Routes>

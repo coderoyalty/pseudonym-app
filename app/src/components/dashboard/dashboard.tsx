c98c0510b278979d862924ca22d0722d5f24226b
Akanni Emmanuel <akannie33@gmail.com>
2024-02-11 21:57:00 +0100

    feat(frontend): add dashboard component
    - update routes.tsx to use /dashboard as the dashboard's page

---
+++ app/src/components/dashboard/dashboard.tsx
@@ -0,0 +1,94 @@
+import {
+  GearIcon,
+  EnvelopeClosedIcon,
+  ArchiveIcon,
+  AvatarIcon,
+  DashboardIcon,
+} from "@radix-ui/react-icons";
+import { IconProps } from "@radix-ui/react-icons/dist/types";
+import React from "react";
+import { Outlet, useLocation } from "react-router-dom";
+
+type NavItem = {
+  icon: React.ForwardRefExoticComponent<
+    IconProps & React.RefAttributes<SVGSVGElement>
+  >;
+  to: string;
+  name: string;
+};
+
+interface SideBarProps extends React.ComponentPropsWithoutRef<"aside"> {
+  items?: NavItem[];
+}
+
+const SideBar: React.FC<SideBarProps> = ({ className, items = [] }) => {
+  return (
+    <aside className={className}>
+      <nav>
+        <ul className="space-y-1">
+          {items.map((item, idx) => (
+            <li key={idx}>
+              <a
+                href={item.to}
+                className="font-medium text-base flex w-full items-center border-r-2 px-2 py-1.5 border-r-transparent text-gray-700 hover:border-r-gray-400"
+              >
+                <span className="flex flex-grow items-center space-x-2">
+                  <item.icon width={18} height={18} />
+                  <span>{item.name}</span>
+                </span>
+              </a>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </aside>
+  );
+};
+
+const Dashboard = () => {
+  const location = useLocation();
+  console.log(location);
+  const items: NavItem[] = [
+    {
+      icon: DashboardIcon,
+      to: "/dashboard",
+      name: "Home",
+    },
+    {
+      icon: EnvelopeClosedIcon,
+      to: "/dashboard/messages",
+      name: "Inbox",
+    },
+    {
+      icon: AvatarIcon,
+      to: "/dashboard/account",
+      name: "Account",
+    },
+    {
+      icon: GearIcon,
+      to: "/dashboard/settings",
+      name: "Settings",
+    },
+    {
+      icon: ArchiveIcon,
+      to: "/dashboard/archive",
+      name: "Archive",
+    },
+  ];
+
+  return (
+    <>
+      <div className="container max-w-[830px] min-h-screen flex">
+        <SideBar
+          className="hidden w-[195px] border-r border-slate-200 shrink-0 py-10 md:block"
+          items={items}
+        />
+        <div className="grow p-0 md:p-10 flex justify-center items-center">
+          <Outlet />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Dashboard;
--- app/src/routes.tsx
+++ app/src/routes.tsx
@@ -2,6 +2,27 @@ import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/home-page";
 import AuthPage from "./pages/AuthPage";
 import NotFoundPage from "./pages/404";
+import Dashboard from "./components/dashboard/dashboard";
+
+import { motion } from "framer-motion";
+
+const DashboardHome = () => {
+  return (
+    <motion.div
+      initial={{ scale: 0, borderRadius: "50%", rotate: 0 }}
+      animate={{
+        rotate: [360, 0, 360],
+        scale: 1,
+        borderRadius: "100%",
+        x: ["0px", "300px", "0px"],
+        backgroundColor: ["#dfdfd3", "#abcdef"],
+      }}
+      className="w-32 h-32 flex justify-center items-center"
+    >
+      Dashboard
+    </motion.div>
+  );
+};
 
 export default function Router() {
   return (
@@ -10,6 +31,9 @@ export default function Router() {
         <Route path="*" element={<NotFoundPage />} />
         <Route path="/" element={<HomePage />} />
         <Route path="/auth" element={<AuthPage />} />
+        <Route path="/dashboard" element={<Dashboard />}>
+          <Route path="*" element={<DashboardHome />} />
+        </Route>
       </Routes>
     </>
   );

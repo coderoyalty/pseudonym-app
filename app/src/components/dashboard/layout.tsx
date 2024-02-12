c2051df180dfcbf3bc1038102fe0599b810d177f
Akanni Emmanuel <akannie33@gmail.com>
2024-02-12 09:53:52 +0100

    feat(frontend): implement mobile navigation dropdown menu in Dashboard layout
    - created a new component `MobileNav` for mobile navigation in the dashboard layout
    - handle outside clicks and close the dropdown menu when clicked outside
    - style the mobile navigation for better UX

--- app/src/components/dashboard/layout.tsx
+++ app/src/components/dashboard/layout.tsx
@@ -1,13 +1,16 @@
+import useClickOutside from "@/hooks/useClickOutside";
 import {
   GearIcon,
   EnvelopeClosedIcon,
   ArchiveIcon,
   AvatarIcon,
   DashboardIcon,
+  CaretDownIcon,
 } from "@radix-ui/react-icons";
 import { IconProps } from "@radix-ui/react-icons/dist/types";
+import { Button } from "@radix-ui/themes";
 import React from "react";
-import { Link, Outlet, useLocation } from "react-router-dom";
+import { Link, Location, Outlet, useLocation } from "react-router-dom";
 
 type NavItem = {
   icon: React.ForwardRefExoticComponent<
@@ -17,11 +20,12 @@ type NavItem = {
   name: string;
 };
 
-interface SideBarProps extends React.ComponentPropsWithoutRef<"aside"> {
+interface NavProps extends React.ComponentPropsWithoutRef<"aside"> {
   items?: NavItem[];
+  location: Location<any>;
 }
 
-const SideBar: React.FC<SideBarProps> = ({ className, items = [] }) => {
+const SideBar: React.FC<NavProps> = ({ className, location, items = [] }) => {
   return (
     <aside className={className}>
       <nav>
@@ -30,7 +34,11 @@ const SideBar: React.FC<SideBarProps> = ({ className, items = [] }) => {
             <li key={idx}>
               <Link
                 to={item.to}
-                className="font-medium text-base flex w-full items-center border-r-2 px-2 py-1.5 border-r-transparent text-gray-700 hover:border-r-gray-400"
+                className={`font-medium text-base flex w-full items-center border-r-2 px-2 py-1.5 border-r-transparent text-gray-700 ${
+                  item.to === location.pathname
+                    ? "border-r-black"
+                    : "hover:border-r-gray-400"
+                }`}
               >
                 <span className="flex flex-grow items-center space-x-2">
                   <item.icon width={18} height={18} />
@@ -45,9 +53,53 @@ const SideBar: React.FC<SideBarProps> = ({ className, items = [] }) => {
   );
 };
 
+const MobileNav: React.FC<NavProps> = ({ className, location, items = [] }) => {
+  const [isOpen, setOpen] = React.useState(false);
+  const ref = React.useRef<HTMLDivElement>(null);
+  useClickOutside(ref, () => setOpen(false));
+
+  return (
+    <>
+      <div className={className} ref={ref}>
+        <Button
+          onClick={() => {
+            setOpen(!isOpen);
+          }}
+          variant="outline"
+          color="gray"
+          className="flex h-10 w-full items-center justify-between rounded-md cursor-pointer border px-2 text-center text-sm font-medium"
+        >
+          Profile <CaretDownIcon />
+        </Button>
+
+        {isOpen && (
+          <ul className="absolute left-0 right-0 z-10 mt-1 space-y-1.5 bg-white p-2 shadow-lg">
+            {items.map((item, idx) => (
+              <li key={idx}>
+                <Link
+                  to={item.to}
+                  className={`flex w-full items-center rounded px-3 py-1.5 font-medium text-base text-slate-900 ${
+                    location.pathname === item.to
+                      ? "bg-slate-100"
+                      : "hover:bg-slate-200"
+                  }`}
+                >
+                  <span className="flex flex-grow items-center space-x-2">
+                    <item.icon width={18} height={18} />
+                    <span>{item.name}</span>
+                  </span>
+                </Link>
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
+    </>
+  );
+};
+
 const DashboardLayout = () => {
   const location = useLocation();
-  console.log(location);
   const items: NavItem[] = [
     {
       icon: DashboardIcon,
@@ -78,8 +130,14 @@ const DashboardLayout = () => {
 
   return (
     <>
+      <MobileNav
+        location={location}
+        className="relative mb-5 block border-b p-4 shadow-inner md:hidden"
+        items={items}
+      />
       <div className="container max-w-[830px] min-h-screen flex">
         <SideBar
+          location={location}
           className="hidden w-[195px] border-r border-slate-200 shrink-0 py-10 md:block"
           items={items}
         />

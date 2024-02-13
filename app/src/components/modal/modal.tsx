25fd01bfcdc3cfa8e6c87078f6be28189f32920d
Akanni Emmanuel <akannie33@gmail.com>
2024-02-13 10:56:20 +0100

    feat(frontend): add animation to modal component
    - add a close icon button for closing the modal
    - add an option (avoidScroll in ModalProps) to disable the scrollbar when modal is open or not

--- app/src/components/modal/modal.tsx
+++ app/src/components/modal/modal.tsx
@@ -1,15 +1,20 @@
 import useClickOutside from "@/hooks/useClickOutside";
+import { Cross2Icon } from "@radix-ui/react-icons";
+import { IconButton } from "@radix-ui/themes";
+import { motion, AnimatePresence } from "framer-motion";
 import React from "react";
 
 interface ModalProps {
   children?: React.ReactNode;
   onClose?: () => void;
   isOpen: boolean;
+  avoidScroll?: boolean;
 }
 
 const Modal: React.FC<ModalProps> = ({
   isOpen,
   children,
+  avoidScroll = false,
   onClose = () => {},
 }) => {
   const [isClosing, _] = React.useState(false);
@@ -19,34 +24,65 @@ const Modal: React.FC<ModalProps> = ({
   });
 
   React.useEffect(() => {
-    if (isOpen) {
-      document.body.style.overflow = "hidden"; // Hide scrollbar when modal is open
-    } else {
-      document.body.style.overflow = ""; // Restore scrollbar when modal is closed
+    // Hide scrollbar when modal is open
+    if (avoidScroll) {
+      if (isOpen) {
+        document.body.style.overflow = "hidden";
+      } else {
+        document.body.style.overflow = "";
+      }
     }
+
+    // Make sure to restore scrollbar on unmount
     return () => {
-      document.body.style.overflow = ""; // Make sure to restore scrollbar on unmount
+      if (avoidScroll) {
+        document.body.style.overflow = "";
+      }
     };
   }, [isOpen]);
 
   return (
     <>
-      {isOpen && (
-        <div
-          className={`fixed inset-0 flex items-center justify-center z-50 ${
-            isClosing ? "opacity-0" : "opacity-100"
-          } transition-opacity duration-300`}
-        >
-          {/* the blur background */}
-          <div className="absolute inset-0 bg-black opacity-50"></div>
-          <div
-            className="relative bg-white p-4 rounded-lg shadow-lg z-50"
-            ref={ref}
+      <AnimatePresence>
+        {isOpen && (
+          <motion.div
+            initial={{
+              opacity: 0,
+            }}
+            animate={{
+              opacity: 100,
+            }}
+            className={`fixed inset-0 flex items-center justify-center z-50 ${
+              isClosing ? "opacity-0" : "opacity-100"
+            } transition-opacity duration-300`}
           >
-            {children}
-          </div>
-        </div>
-      )}
+            {/* the blur background */}
+            <div className="absolute inset-0 bg-slate-950 opacity-50"></div>
+            <motion.div
+              initial={{
+                scale: 0,
+              }}
+              animate={{
+                scale: 1,
+              }}
+              className="relative bg-white p-4 rounded-lg shadow-lg z-50"
+              ref={ref}
+            >
+              <div className="flex justify-end mb-1">
+                <IconButton
+                  variant="outline"
+                  color="mint"
+                  className="cursor-pointer"
+                  onClick={onClose}
+                >
+                  <Cross2Icon width={16} height={16} />
+                </IconButton>
+              </div>
+              {children}
+            </motion.div>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </>
   );
 };

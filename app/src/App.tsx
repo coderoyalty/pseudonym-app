7684acfb1641548fff38a08b4006f502ee0fc314
Akanni Emmanuel <akannie33@gmail.com>
2024-02-15 19:42:32 +0100

    chore(app): add shadcn-ui toast component

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -4,12 +4,14 @@ import { Theme } from "@radix-ui/themes";
 import Router from "./routes";
 import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./contexts/auth";
+import { Toaster } from "@/components/ui/toaster";
 
 function App() {
   return (
     <>
       <AuthProvider>
         <Theme>
+          <Toaster />
           <BrowserRouter>
             <Router />
           </BrowserRouter>

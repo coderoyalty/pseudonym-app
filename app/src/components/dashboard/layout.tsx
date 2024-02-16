34d0dbbad03453fe70e5c0008d0ebc3c105997c5
Akanni Emmanuel <akannie33@gmail.com>
2024-02-16 15:58:11 +0100

    refactor(app): update dashboard layout and authentication page
    Changes include:
    - updated the dashboard layout to include a header component with a logout dropdown menu
    - removed the "Home" link from the sidebar navigation in the dashboard layout
    - improved the logic to handle redirection to the dashboard when the user is logged in

--- app/src/components/dashboard/layout.tsx
+++ app/src/components/dashboard/layout.tsx
@@ -3,18 +3,16 @@ import {
   ArchiveIcon,
   AvatarIcon,
   DashboardIcon,
-  HomeIcon,
+  CaretDownIcon,
 } from "@radix-ui/react-icons";
-import { Outlet, useLocation } from "react-router-dom";
+import { Link, Outlet, useLocation, useNavigate } from "react-router-dom";
 import MobileNav from "./mobile-nav";
 import SideBar, { NavItem } from "./sidebar";
+import { Button, DropdownMenu } from "@radix-ui/themes";
+import { useAuth } from "@/contexts/auth";
+import axios from "@/api/axios";
 
 const items: NavItem[] = [
-  {
-    icon: HomeIcon,
-    to: "/",
-    name: "Home",
-  },
   {
     icon: DashboardIcon,
     to: "/dashboard",
@@ -37,11 +35,45 @@ const items: NavItem[] = [
   },
 ];
 
+const DashboardHeader = () => {
+  const { signout } = useAuth();
+  const navigate = useNavigate();
+  const handleLogout = () => {
+    axios.post("/auth/logout").then(() => {
+      signout();
+      navigate("/auth");
+    });
+  };
+  return (
+    <>
+      <div className="bg-slate-950 py-4 flex justify-between items-center px-4 md:px-20">
+        <Link to="/" className="text-xl text-white">
+          Pseudonym
+        </Link>
+        <DropdownMenu.Root>
+          <DropdownMenu.Trigger>
+            <Button variant="soft">
+              Menu
+              <CaretDownIcon />
+            </Button>
+          </DropdownMenu.Trigger>
+          <DropdownMenu.Content>
+            <DropdownMenu.Item color="red" onClick={handleLogout}>
+              Logout
+            </DropdownMenu.Item>
+          </DropdownMenu.Content>
+        </DropdownMenu.Root>
+      </div>
+    </>
+  );
+};
+
 const DashboardLayout = () => {
   const location = useLocation();
 
   return (
     <>
+      <DashboardHeader />
       <MobileNav location={location} items={items} />
       <div className="container max-w-[830px] min-h-screen flex">
         <SideBar location={location} items={items} />

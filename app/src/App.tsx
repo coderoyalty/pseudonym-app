1da277978464593be01f265cf3ec86acfd33433c
Akanni Emmanuel <akannie33@gmail.com>
2024-02-16 17:16:49 +0100

    refactor: refactor App component to utilize Application component
    The App component is refactored to use the Application component, which encapsulates the main application logic and structure

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -3,19 +3,37 @@ import "@radix-ui/themes/styles.css";
 import { Theme } from "@radix-ui/themes";
 import Router from "./routes";
 import { BrowserRouter } from "react-router-dom";
-import { AuthProvider } from "./contexts/auth";
+import { AuthProvider, useAuth } from "./contexts/auth";
 import { Toaster } from "@/components/ui/toaster";
+import React from "react";
+import axios from "./api/axios";
+
+function Application() {
+  const { signout } = useAuth();
+
+  React.useEffect(() => {
+    axios.get("/users/me/stats").catch((_err) => {
+      signout();
+    });
+  }, []);
+
+  return (
+    <>
+      <Theme>
+        <Toaster />
+        <BrowserRouter>
+          <Router />
+        </BrowserRouter>
+      </Theme>
+    </>
+  );
+}
 
 function App() {
   return (
     <>
       <AuthProvider>
-        <Theme>
-          <Toaster />
-          <BrowserRouter>
-            <Router />
-          </BrowserRouter>
-        </Theme>
+        <Application />
       </AuthProvider>
     </>
   );

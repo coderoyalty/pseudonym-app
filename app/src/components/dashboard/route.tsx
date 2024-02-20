7fc2a972eb77259535a2d4c454c8a36c08955ee9
Akanni Emmanuel <akannie33@gmail.com>
2024-02-20 14:18:32 +0100

    refactor(app): move dashboard/home.tsx to pages directory

--- app/src/components/dashboard/route.tsx
+++ app/src/components/dashboard/route.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Route } from "react-router-dom";
-import DashboardHome from "./home";
+import DashboardHome from "@/pages/dashboard/home";
 import DashboardLayout from "./layout";
 import NotFoundPage from "@/pages/404";
 import ProtectedRoute from "./protected-route";

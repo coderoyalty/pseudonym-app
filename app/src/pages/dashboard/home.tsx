6ac6034013f8c54707aadc45971fb13456437a50
Akanni Emmanuel <akannie33@gmail.com>
2024-04-02 20:27:48 +0100

    feat(ui): add loading skeleton to dashboard info page

--- app/src/pages/dashboard/home.tsx
+++ app/src/pages/dashboard/home.tsx
@@ -4,23 +4,34 @@ import CopyableInput from "@/components/copyable-input";
 import { useAuth } from "@/contexts/auth";
 import axios from "@/api/axios";
 import { InboxContent } from "@/components/dashboard/inbox/inbox-pagination";
+import { Skeleton } from "@/components/ui/skeleton";
 
 const UserStatsPanel = ({
   messageCount,
   archivedCount,
+  loading = false,
 }: {
   messageCount: number;
   archivedCount: number;
+  loading?: boolean;
 }) => {
   return (
     <>
       <div className="flex flex-col sm:flex-row justify-evenly gap-0 sm:gap-2 divide-y sm:divide-y-0 divide-x-0 sm:divide-x border-b">
         <div className="flex flex-col items-center justify-center gap-2 p-4">
-          <span className="text-5xl">{messageCount}</span>
+          {loading ? (
+            <Skeleton className="h-16 w-full" />
+          ) : (
+            <span className="text-5xl">{messageCount}</span>
+          )}
           <span>messages received</span>
         </div>
         <div className="flex flex-col items-center justify-center gap-2 p-4">
-          <span className="text-5xl">{archivedCount}</span>
+          {loading ? (
+            <Skeleton className="h-16 w-full" />
+          ) : (
+            <span className="text-5xl">{archivedCount}</span>
+          )}
           <span>messages archived</span>
         </div>
       </div>
@@ -38,13 +49,20 @@ const DashboardHome: React.FC = () => {
   const { user, signin } = useAuth();
 
   const [data, setData] = React.useState<HomeProps>(null!);
+  const [isLoading, setLoading] = React.useState(false);
   const url = `https://pseudonym-app.vercel.app/${user?.username}`;
 
   React.useEffect(() => {
-    axios.get("/users/me/stats").then((response) => {
-      signin(response.data.user);
-      setData(response.data);
-    });
+    setLoading(true);
+    axios
+      .get("/users/me/stats")
+      .then((response) => {
+        signin(response.data.user);
+        setData(response.data);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
   }, []);
 
   return (
@@ -53,6 +71,7 @@ const DashboardHome: React.FC = () => {
         <UserStatsPanel
           messageCount={data?.messagesCount}
           archivedCount={data?.archivedCount}
+          loading={isLoading}
         />
         <div className="p-4">
           <h1 className="font-medium">
